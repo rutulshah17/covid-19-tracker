ffc8a435311c8fe8235d4e7c3ccaf1abaf1e8797
Rutul Shah <rutulshah17@gmail.com>
2020-05-04 16:33:45 -0400

    Cards component ready with styles, getting data via api and displaying in material cards

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import Cards from './components/Cards/Cards.component';
 import Chart from './components/Chart/Chart.component'; 
 import CountryPicker from './components/CountryPicker/CountryPicker.component'; 
 
+import './app.css'
+
+import { fetchApiData } from  './api/api';
 
 class App extends React.Component {
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchApiData();
+
+        this.setState ( { data: fetchedData } );
+
+    }
+
     render() {
+
+        const { data } = this.state;
+
         return(
-            <div>
-                <Cards/>
-                <Chart/>
-                <CountryPicker/>    
+            <div className='app-container'>
+                <Cards data={data} />
+                <CountryPicker />    
+                <Chart />
             </div>
         )
     }
--- src/components/Cards/Cards.component.jsx
+++ src/components/Cards/Cards.component.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
-import './Cards.styles.css'
 
-const Cards = () => (
-    <div>
-        <h3> Cards </h3>
-    </div>
-);
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-export default Cards
+import './Cards.styles.css';
+
+const Cards = ( { data: { confirmed, recovered, deaths, lastUpdate } } ) => {
+
+	if(!confirmed) {
+		return 'Loading...'
+	}
+	
+	return (
+		<div className='cards-container'>
+			<Grid container spacing={3} justify='center'>
+				<Grid item component={Card} xs={12} md={3} className='cards-grid-item infected' >
+					<CardContent>
+						<Typography color='textSecondary' gutterBottom> Infected </Typography>
+						<Typography variant='h5'> 
+							<CountUp 
+								start={0}
+								end={confirmed.value}
+								duration={1.5}
+								separator=','
+							/>
+						</Typography>
+						<Typography color='textSecondary'> { new Date(lastUpdate).toDateString() } </Typography>
+						<Typography variant='body2'> Number of active cases of COVID-19 </Typography>
+					</CardContent>
+				</Grid>
+
+				<Grid item component={Card} xs={12} md={3} className='cards-grid-item recovered' >
+					<CardContent>
+						<Typography color='textSecondary' gutterBottom> Recovered </Typography>
+						<Typography variant='h5'> 
+							<CountUp 
+								start={0}
+								end={recovered.value}
+								duration={1.0}
+								separator=','
+							/>
+						</Typography>
+						<Typography color='textSecondary'> { new Date(lastUpdate).toDateString() } </Typography>
+						<Typography variant='body2'> Number of active recoveries from COVID-19 </Typography>
+					</CardContent>
+				</Grid>
+
+				<Grid item component={Card} xs={12} md={3} className='cards-grid-item deaths' >
+					<CardContent>
+						<Typography color='textSecondary' gutterBottom> Deaths </Typography>
+						<Typography variant='h5'> 
+							<CountUp 
+								start={0}
+								end={deaths.value}
+								duration={1.5}
+								separator=','
+							/>
+						</Typography>
+						<Typography color='textSecondary'> { new Date(lastUpdate).toDateString() } </Typography>
+						<Typography variant='body2'> Number of deaths caused by COVID-19 </Typography>
+					</CardContent>
+				</Grid>
+
+			</Grid>
+		</div>
+)
+};
+
+export default Cards;
